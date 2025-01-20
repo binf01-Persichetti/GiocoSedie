03955acaaf5e42fb0be0cebcfc4f1eaf9211c30d
LorenzoPersi <105106983+binf01-Persichetti@users.noreply.github.com>
2025-01-20 16:20:33 +0100

    Update TestGiocoSedie.java
    Aggiunta la possibilità di scegliere il numero di partecipanti

--- src/main/java/giocosedie/TestGiocoSedie.java
+++ src/main/java/giocosedie/TestGiocoSedie.java
@@ -15,32 +15,31 @@ import java.util.logging.Logger;
  */
 import java.util.logging.Logger;
 
+import java.util.Scanner;
+
 public class TestGiocoSedie {
-    private final static int NUMSEDIE = 15;
-    private static Logger logger = Logger.getLogger("GiocoSedie.TestGiocoSedie");
-    String nomeFile;
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Posto sedie[] = new Posto[NUMSEDIE];
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Inserisci il numero di partecipanti: ");
+        int numPartecipanti = scanner.nextInt();
 
-	for (int k = 0; k < sedie.length; k++)
-		sedie[k] = new Posto();
+        Posto sedie[] = new Posto[numPartecipanti - 1];
 
-	Display display = new Display(sedie);
-	//System.out.println("Sto facendo partire il Display.");
-        logger.info("Sto facendo partire il Display.\n");
-	display.start();
+        for (int k = 0; k < sedie.length; k++)
+            sedie[k] = new Posto();
 
-	Partecipante array[] = new Partecipante[NUMSEDIE+1];
-	for (int i = 0; i < NUMSEDIE + 1; i++) {
-		array[i] = new Partecipante(sedie);
-                //System.out.println("Sto facendo partire il thread n." + array[i].getId());
-                logger.info("Sto facendo partire il thread id: " + array[i].getId()+" name: "+array[i].getName()+"\n");
-                array[i].start();
-                }
-	}
+        Display display = new Display(sedie);
+        display.start();
+
+        Partecipante array[] = new Partecipante[numPartecipanti];
+        for (int i = 0; i < numPartecipanti; i++) {
+            array[i] = new Partecipante(sedie);
+            array[i].start();
+        }
+        scanner.close();
+    }
+}
+
         public void scrivi(){
         BufferedWriter br=null;
         
