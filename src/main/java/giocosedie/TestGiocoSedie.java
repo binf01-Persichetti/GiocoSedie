6cd33d3de574b25e1753875ef05adbcb94edfdf9
LorenzoPersi <105106983+binf01-Persichetti@users.noreply.github.com>
2025-01-21 12:56:48 +0100

    Update TestGiocoSedie.java

--- src/main/java/giocosedie/TestGiocoSedie.java
+++ src/main/java/giocosedie/TestGiocoSedie.java
@@ -8,45 +8,54 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import
 
 /**
  *
  * @author LP
  */
 
-import java.util.Scanner;
-
 public class TestGiocoSedie {
     private final static int NUMSEDIE = 15;
     private static Logger logger = Logger.getLogger("GiocoSedie.TestGiocoSedie");
     String nomeFile= "Persichetti_file.txt";
-
+   
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Posto sedie[] = new Posto[NUMSEDIE];
+        Scanner scanner = new Scanner(System.in);
 
-        for (int k = 0; k < sedie.length; k++)
-                sedie[k] = new Posto();
+        System.out.print("Inserisci il numero di partecipanti: ");
+        int numPartecipanti = scanner.nextInt();
+
+        // Determina il numero di sedie: sempre una in meno rispetto ai partecipanti
+        int numSedie = numPartecipanti - 1;
+        Posto[] sedie = new Posto[numSedie];
+
+        // Inizializzazione delle sedie
+        for (int k = 0; k < sedie.length; k++) {
+            sedie[k] = new Posto();
+        }
 
         Display display = new Display(sedie);
-        //System.out.println("Sto facendo partire il Display.");
-        logger.info("Sto facendo partire il Display.\n");
+        logger.info("Sto facendo partire il Display.");
         display.start();
 
-        Partecipante array[] = new Partecipante[NUMSEDIE+1];
-        for (int i = 0; i < NUMSEDIE + 1; i++) {
-                array[i] = new Partecipante(sedie);
-                //System.out.println("Sto facendo partire il thread n." + array[i].getId());
-                logger.info("Sto facendo partire il thread id: " + array[i].getId()+" name: "+array[i].getName()+"\n");
-                array[i].start();
-                }
+        Partecipante[] partecipanti = new Partecipante[numPartecipanti];
+
+        for (int i = 0; i < numPartecipanti; i++) {
+            partecipanti[i] = new Partecipante(sedie);
+            logger.info("Sto facendo partire il thread id: " + partecipanti[i].getId() + " name: " + partecipanti[i].getName());
+            partecipanti[i].start();
         }
+
+        scanner.close();
+}
 
     public void scrivi(){
         BufferedWriter br=null;
-
+       
         try {
             //1) apro il file
             br = new BufferedWriter(
@@ -55,10 +64,10 @@ public class TestGiocoSedie {
             br.write("File in output");
             br.write("\n\r");
             //3) svuoto il buffer e salvo nel file i dati
-            br.flush();         
+            br.flush();        
         } catch (IOException ex) {
             //Logger.getLogger(Scrittore.class.getName()).log(Level.SEVERE, null, ex);
-                System.err.println("Stampa l'ID");
+System.err.println("Stampa l'ID");
         }
         finally{
             if (br!=null)
@@ -67,15 +76,15 @@ public class TestGiocoSedie {
                     br.close();
             } catch (IOException ex) {
                 //Logger.getLogger(Scrittore.class.getName()).log(Level.SEVERE, null, ex);
-                        System.err.println("Stampa");
+System.err.println("Stampa");
             }
-
+               
         }
     }
-
+   
     public void scrivi(String messaggio, boolean append){
         BufferedWriter br=null;
-
+       
         try {
             //1) apro il file
             br = new BufferedWriter(
@@ -84,10 +93,10 @@ public class TestGiocoSedie {
             br.write(messaggio);
             br.write("\n\r");
             //3) svuoto il buffer e salvo nel file i dati
-            br.flush();         
+            br.flush();        
         } catch (IOException ex) {
             //Logger.getLogger(Scrittore.class.getName()).log(Level.SEVERE, null, ex);
-                System.err.println(ex.getMessage());
+System.err.println(ex.getMessage());
         }
         finally{
             if (br!=null)
@@ -96,9 +105,9 @@ public class TestGiocoSedie {
                     br.close();
             } catch (IOException ex) {
                 //Logger.getLogger(Scrittore.class.getName()).log(Level.SEVERE, null, ex);
-                        System.err.println(ex.getMessage());
+System.err.println(ex.getMessage());
             }
-
+               
         }
     }
 }
